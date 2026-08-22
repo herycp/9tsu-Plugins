1c442eb8055ce747b10ec5ae4c5e362fd91dbac1
Agus Hariono <hery_cp@yahoo.co.id>
2026-08-22 18:08:15 +0700

    Update Dramacool.kt

--- DramacoolProvider/src/main/kotlin/com/drmclmy/Dramacool.kt
+++ DramacoolProvider/src/main/kotlin/com/drmclmy/Dramacool.kt
@@ -1,10 +1,6 @@
 package com.drmclmy
 
-import android.util.Log
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.nodes.Element
@@ -34,38 +30,40 @@ class Dramacool : MainAPI() {
         val title = selectFirst("a")?.attr("title") ?: return null
         val href = fixUrlNull(selectFirst("a")?.attr("href")) ?: return null
         val posterUrl = fixUrlNull(selectFirst("img")?.attr("src"))
-        return newAnimeSearchResponse(title, LoadUrl(href, posterUrl).toJson()) {
+        return newAnimeSearchResponse(title, href, TvType.AsianDrama) {
             this.posterUrl = posterUrl
         }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val searchTitle = query.createSlug()
+        // Buat slug sederhana
+        val searchTitle = query.replace(" ", "-")
         val url = "$mainUrl/search/$searchTitle"
         val document = app.get(url, referer = "$mainUrl/").document
         return document.select("#drama div.card").mapNotNull { it.toSearchResult() }
     }
 
     override suspend fun load(url: String): LoadResponse? {
-        val d = tryParseJson(url) ?: return null
-        val document = app.get(d.url, referer = "$mainUrl/").document
+        val document = app.get(url, referer = "$mainUrl/").document
 
         val title = document.selectFirst("h1")?.text()?.trim() ?: return null
+
+        val posterUrl = document.selectFirst("img.poster")?.attr("src")?.let { fixUrl(it) }
 
         val actors = document.select("div.slider div.img-container").map {
             Actor(it.select("div.bottom-right").text(), it.select("img").attr("src"))
         }
 
         val episodes = document.select("div.epdiv").mapNotNull { el ->
-            val name = el.selectFirst("a")?.text()?.substringAfter("Episode")?.trim()
-            val rawHref = el.selectFirst("a")?.attr("href") ?: ""
-            val href = "$mainUrl/$rawHref"
-            Episode(href, "Episode $name")
+            val name = el.selectFirst("a")?.text()?.substringAfter("Episode")?.trim() ?: return@mapNotNull null
+            val rawHref = el.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val href = fixUrl(rawHref)
+            newEpisode(href, "Episode $name")
         }.reversed()
 
         return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
-            posterUrl = d.posterUrl
-            addActors(actors)
+            this.posterUrl = posterUrl
+            this.actors = actors
         }
     }
 
