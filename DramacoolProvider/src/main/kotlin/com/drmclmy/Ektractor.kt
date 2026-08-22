13eabf90699bf3f07c9aee1e9658e7917bfd7186
Agus Hariono <hery_cp@yahoo.co.id>
2026-08-22 17:28:11 +0700

    Create Ektractor.kt

---
+++ DramacoolProvider/src/main/kotlin/com/drmclmy/Ektractor.kt
@@ -0,0 +1,13 @@
+package com.drmclmy
+
+import com.lagradost.cloudstream3.extractors.MixDrop
+import com.lagradost.cloudstream3.extractors.VidhideExtractor
+
+class dlions : VidhideExtractor() {
+    override var name = "Dlions"
+    override var mainUrl = "https://dlions.pro"
+}
+
+class MixDropSi : MixDrop() {
+    override var mainUrl = "https://mixdrop.si"
+}
