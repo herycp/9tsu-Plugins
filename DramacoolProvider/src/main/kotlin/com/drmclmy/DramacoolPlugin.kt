6483d62b29c7c877c5a364b26e0b1f4747145c73
Agus Hariono <hery_cp@yahoo.co.id>
2026-08-22 17:21:08 +0700

    Update and rename NineTsufixPlugin.kt to DramacoolPlugin.kt

---
+++ DramacoolProvider/src/main/kotlin/com/drmclmy/DramacoolPlugin.kt
@@ -0,0 +1,18 @@
+package com.drmclmy
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.Dwish
+import com.lagradost.cloudstream3.extractors.StreamTape
+
+@CloudstreamPlugin
+class DramacoolPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Dramacool())
+        registerExtractorAPI(Dwish())
+        registerExtractorAPI(dlions())
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(MixDropSi())
+    }
+}
