8faeb16b615bafaf2c411f5be312d63701b79a2b
Agus Hariono <hery_cp@yahoo.co.id>
2026-08-26 13:47:29 +0700

    Update NineTsufixPlugin.kt

--- 9tsufixProvider/src/main/kotlin/com/ninetsufix/NineTsufixPlugin.kt
+++ 9tsufixProvider/src/main/kotlin/com/ninetsufix/NineTsufixPlugin.kt
@@ -3,11 +3,20 @@ package com.ninetsufix
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
 
 @CloudstreamPlugin
-class NineTsuinPlugin : Plugin() {
+class NineTsufixPlugin : Plugin() {
     override fun load(context: Context) {
         // Mendaftarkan provider 9tsu
         registerMainAPI(NineTsuFixProvider())
+        
+        // Mendaftarkan pengaturan untuk memilih domain
+        this.openSettings = { ctx ->
+            NineTsuPrefs.showDomainDialog(ctx as AppCompatActivity) {
+                // Setelah menyimpan preferensi, user bisa refresh manual
+                // atau kita bisa trigger reload jika diperlukan
+            }
+        }
     }
 }
