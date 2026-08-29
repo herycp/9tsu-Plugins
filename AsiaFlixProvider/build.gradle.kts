e6385291c705fbede2136f8f75f1a73e85316dde
Agus Hariono <hery_cp@yahoo.co.id>
2026-08-29 16:00:01 +0700

    Update build.gradle.kts

--- AsiaFlixProvider/build.gradle.kts
+++ AsiaFlixProvider/build.gradle.kts
@@ -1,38 +1,47 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+import com.lagradost.cloudstream3.plugins.CloudstreamPluginConfiguration
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    }
 }
 
-// Use an integer for version numbers
-version = 17
-
-cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "asiaflix.net provider"
-    authors = listOf("Herycp")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
 
 android {
-    buildFeatures {
-        buildConfig = true
-        viewBinding = true
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("com.github.recloudstream:cloudstream:pre-release")
+    implementation("org.jsoup:jsoup:1.15.3")
+}
+
+cloudstream {
+    setPluginConfig(
+        CloudstreamPluginConfiguration(
+            name = "Asiaflix",
+            pluginClassName = "com.asiaflix.AsiaflixPlugin",
+            authors = listOf("You"),
+            version = 1
+        )
+    )
 }
