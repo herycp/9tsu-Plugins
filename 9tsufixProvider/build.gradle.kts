b72d7aa9d018d11ee25b8866c126953312b22983
Agus Hariono <hery_cp@yahoo.co.id>
2026-09-12 16:35:59 +0700

    Update build.gradle.kts

--- 9tsufixProvider/build.gradle.kts
+++ 9tsufixProvider/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
@@ -31,8 +33,7 @@ cloudstream {
 }
 kotlin {
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        jvmTarget.set(JvmTarget.JVM_11)
     }
     kotlinOptions {
         jvmTarget = "11"
